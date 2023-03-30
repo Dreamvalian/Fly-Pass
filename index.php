82101667bfa09128fded80214ea2d11c961e03fe
Hanif Nugraha <nailavmaerd@gmail.com>
2023-03-30 20:53:02 +0700

    update about page

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.css.min">
     <link rel="stylesheet" href="styles/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -33,8 +33,27 @@
         <div class="right-hero">
         </div>
     </div>
+    <div class="about">
+        <div class="marquee-text">
+            <span>
+                As a versatile and beloved beverage, Whether you prefer red, white, rosé, or sparkling, there's a wine out there to suit every taste and occasion. 🍷
+            </span>
+        </div>
+        <div class="about-content">
+            <div class="content">
+                <h3>
+                    Leading vault of Wine Product:
+                </h3>
+                <p>
+                    With VinoVault, you can easily add new wines to your inventory, update wine quantities, and view detailed reports on your inventory and sales. Our user-friendly interface and powerful features make it easy to manage your wine warehouse, so you can focus on what you do best - providing your customers with the finest wines and the best service.
+                    <br><br>
+                    Whether you are a small independent wine retailer or a large wine distributor, VinoVault is the perfect tool to help you manage your wine inventory and sales more efficiently. Sign up now and experience the benefits of VinoVault for yourself!
+                </p><br>
+            </div>
+            <img src="./img/about-image1.jpg">
+        </div>
 
-
+    </div>
 </body>
 
 </html>
