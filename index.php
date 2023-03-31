3c2e346939de8ec1f54c43c0aa4f5c6e9247b4d0
Hanif Nugraha <nailavmaerd@gmail.com>
2023-04-01 00:27:37 +0700

    Add Section in Index

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (isset($_POST['search'])) {
 } else if (isset($_POST['search'])) {
     $keyword = $_POST['keyword'];
     $q = "SELECT * FROM wines ";
-} 
+}
 
 $result = mysqli_query($conn, $q);
 
@@ -34,8 +34,7 @@ $result = mysqli_query($conn, $q);
 <body>
 
     <!-- Hero -->
-
-    <div class="hero">
+    <section class="hero">
         <div class="left-hero">
             <h1>Vino Vault</h1>
             <h3>A Nectar of Truth - In Vino Veritas</h3>
@@ -52,68 +51,73 @@ $result = mysqli_query($conn, $q);
         </div>
         <div class="right-hero">
         </div>
-    </div>
+    </section>
+
 
     <!-- About -->
-
-    <div class="about">
-        <div class="marquee-text">
-            <span>
-                As a versatile and beloved beverage, Whether you prefer red, white, rosé, or sparkling, there's a wine
-                out there to suit every taste and occasion. 🍷
-            </span>
-        </div>
-        <div class="about-content">
-            <div class="content">
-                <h3>
-                    Leading vault of Wine Product:
-                </h3>
-                <p>
-                    With VinoVault, you can easily add new wines to your inventory, update wine quantities, and view
-                    detailed reports on your inventory and sales. Our user-friendly interface and powerful features make
-                    it easy to manage your wine warehouse, so you can focus on what you do best - providing your
-                    customers with the finest wines and the best service.
-                    <br><br>
-                    Whether you are a small independent wine retailer or a large wine distributor, VinoVault is the
-                    perfect tool to help you manage your wine inventory and sales more efficiently. Sign up now and
-                    experience the benefits of VinoVault for yourself!
-                </p><br>
+    <section class="about">
+        <div class="about">
+            <div class="marquee-text">
+                <span>
+                    As a versatile and beloved beverage, Whether you prefer red, white, rosé, or sparkling, there's a wine
+                    out there to suit every taste and occasion. 🍷
+                </span>
             </div>
-            <img src="./img/about-image1.jpg">
+            <div class="about-content">
+                <div class="content">
+                    <h3>
+                        Leading vault of Wine Product:
+                    </h3>
+                    <p>
+                        With VinoVault, you can easily add new wines to your inventory, update wine quantities, and view
+                        detailed reports on your inventory and sales. Our user-friendly interface and powerful features make
+                        it easy to manage your wine warehouse, so you can focus on what you do best - providing your
+                        customers with the finest wines and the best service.
+                        <br><br>
+                        Whether you are a small independent wine retailer or a large wine distributor, VinoVault is the
+                        perfect tool to help you manage your wine inventory and sales more efficiently. Sign up now and
+                        experience the benefits of VinoVault for yourself!
+                    </p><br>
+                </div>
+                <img src="./img/about-image1.jpg">
+            </div>
         </div>
-    </div>
+    </section>
+
 
     <!-- Search -->
+    <section class="search">
+        <div class="search">
+            <form method="POST" class="search-item">
+                <div class="mb-3">
+                    <label for="search" class="form-label">Search Item</label>
+                    <input type="search" name="keyword" class="form-control">
+                </div>
+                <button name="search" type="submit" class="btn btn-primary">Search</button>
+            </form>
 
-    <div class="search">
-        <form method="POST" class="search-item">
-            <div class="mb-3">
-                <label for="search" class="form-label">Search Item</label>
-                <input type="search" name="keyword" class="form-control">
-            </div>
-            <button name="search" type="submit" class="btn btn-primary">Search</button>
-        </form>
+            <div class="card-grid">
+                <?php if ($result && mysqli_num_rows($result) > 0) {
 
-        <div class="card-grid">
-            <?php if ($result && mysqli_num_rows($result) > 0) {
-
-                while ($row = mysqli_fetch_assoc($result)) { ?>
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $row['name'] ?></h5>
-                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $row['type'] ?></h6>
-                            <p class="card-text"><?php echo $row['description'] ?></p>
-                            <h6 class="card-price"> <?php echo $row['price'] ?> </h6>
-                            <h6 class="card-quantity"><?php echo $row['quantity'] ?></h6>
+                    while ($row = mysqli_fetch_assoc($result)) { ?>
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $row['name'] ?></h5>
+                                <h6 class="card-subtitle mb-2 text-muted"><?php echo $row['type'] ?></h6>
+                                <p class="card-text"><?php echo $row['description'] ?></p>
+                                <h6 class="card-price"> <?php echo $row['price'] ?> </h6>
+                                <h6 class="card-quantity"><?php echo $row['quantity'] ?></h6>
+                            </div>
                         </div>
-                    </div>
-            <?php }
-            } else {
-                echo "no result found !";
-            } ?>
+                <?php }
+                } else {
+                    echo "no result found !";
+                } ?>
 
+            </div>
         </div>
-    </div>
+    </section>
+
 </body>
 
 </html>
