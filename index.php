eb7d4c745a30431a995c68af55dbf13c15490027
Hanif Nugraha <nailavmaerd@gmail.com>
2023-03-31 15:49:34 +0700

    Create Search page

--- index.php
+++ index.php
@@ -52,6 +52,24 @@
             </div>
             <img src="./img/about-image1.jpg">
         </div>
+    </div>
+    <div class="search">
+        <form class="search-item">
+            <div class="mb-3">
+                <input type="search" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Search</button>
+        </form>
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
 
     </div>
 </body>
