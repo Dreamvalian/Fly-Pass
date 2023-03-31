bd989cc3dab9397be31a7dc87664be24065aaff5
Hanif Nugraha <nailavmaerd@gmail.com>
2023-03-31 16:13:34 +0700

    Update Folder

--- index.php
+++ index.php
@@ -56,6 +56,7 @@
     <div class="search">
         <form class="search-item">
             <div class="mb-3">
+                <label for="search" class="form-label">Search Item</label>
                 <input type="search" class="form-control">
             </div>
             <button type="submit" class="btn btn-primary">Search</button>
@@ -66,7 +67,7 @@
                 <h5 class="card-title">Card title</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Card link</a><br>
                 <a href="#" class="card-link">Another link</a>
             </div>
         </div>
