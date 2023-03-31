8eee68f7905242b42a4550ccef5c817357526313
Hanif Nugraha <nailavmaerd@gmail.com>
2023-04-01 01:49:07 +0700

    update login and register

---
+++ script.js
@@ -0,0 +1,12 @@
+const
+    container = document.querySelector(".container"),
+    pwFields = document.querySelectorAll(".password"),
+    signUp = document.querySelector(".signup-link"),
+    login = document.querySelector(".login-link");
+
+signUp.addEventListener("click", () => {
+    container.classList.add("active");
+});
+login.addEventListener("click", () => {
+    container.classList.remove("active");
+});
